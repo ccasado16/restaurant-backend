b224edea2c5df32a5a32779280c902bfc4baeda0
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-05-23 11:31:03 -0500

    Relate payment with a order

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from payments.models import Payment
 from products.models import Product
 from tables.models import Table
 
@@ -12,6 +13,9 @@ class Order(models.Model):
     table = models.ForeignKey(Table, on_delete=models.SET_NULL, null=True, blank=True)
     product = models.ForeignKey(
         Product, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    payment = models.ForeignKey(
+        Payment, on_delete=models.CASCADE, null=True, blank=True
     )
     status = models.CharField(max_length=50, choices=statuses)
     created_at = models.DateTimeField(auto_now_add=True)
