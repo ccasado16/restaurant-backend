5afce6ea4a7a9cd519b4496fceafe5b68144882b
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-05-23 12:44:46 -0500

    Add drf_yasg for api documentation

--- backend/urls.py
+++ backend/urls.py
@@ -16,10 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from users.api.router import router as user_router
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+        description="API docs",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    path(
+        "docs/",
+        schema_view.with_ui(
+            "swagger",
+        ),
+        name="schema-swagger-ui",
+    ),  # api documentation
     path("admin/", admin.site.urls),
     path("api/", include("users.api.router")),  # auth/me
     path("api/", include(user_router.urls)),
